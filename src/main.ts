e1a2ea42a7e09b89a8e9a962ff033f7dac166695
adamchiche <adamchiche06@icloud.com>
2025-06-24 14:35:16 +0200

    init page vue

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
+import router from './router/router'
 
+createApp(App).use(router).mount('#app')
 createApp(App).mount('#app')
