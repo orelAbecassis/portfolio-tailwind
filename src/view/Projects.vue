a231547d199d3aea1105e6a660ab15b73a37f3fd
lenny <lennyrappro@gmail.com>
2025-06-27 14:16:21 +0200

    🔄 Remplacement du composant Projects par NotionProjects et ajout de la logique de chargement des projets depuis l'API.

--- src/view/Projects.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <section class="min-h-screen bg-gradient-to-br from-purple-300 via-pink-200 to-indigo-100 flex items-center justify-center">
-    <div>
-      <!-- Contenu existant -->
-    </div>
-  </section>
-</template>
